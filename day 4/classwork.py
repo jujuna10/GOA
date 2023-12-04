1d373c78fd8f110556cd3a5ceb6438a03ceaca8c
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-04 21:36:02 +0400

    day 4 folder

---
+++ day 4/classwork.py
@@ -0,0 +1,28 @@
+# age = 32                        #int
+# name = "nika"                   #string
+
+
+
+
+
+# print  ("great progress")
+
+
+
+
+# salary = 50000
+# role = "analyst"
+# age = 29
+# print (salary)
+
+
+
+
+
+user_name = input("what's your name:")
+print ("his name is" + user_name)
+
+
+
+user_age = input ("how old are you:")
+print (user_name + " is  " + user_age + " years old")
---
+++ day 4/homework.py
@@ -0,0 +1,61 @@
+# mother = 42
+# father = 52
+# brother = 24
+# i = 16
+
+
+
+# new_age_mother = (mother + 20)
+# new_age_father = (father + 20)
+# new_age_brother = (brother + 20)
+# new_age_i = (i + 20)
+
+
+
+# print(new_age_mother)
+# print(new_age_father)
+# print(new_age_brother)
+# print(new_age_i)
+
+
+
+father = input("enter your father's name:")
+mother = input("enter yout mother's name:")
+brother = input("enter your brother's name:")
+i = input("enter my name:")
+
+
+
+
+
+father_age = input("enter your father's age:")
+mother_age = input("enter yout mother's age:")
+brother_age = input("enter your brother's age:")
+i_age = input("enter my age:")
+
+
+
+
+
+print(father)
+print(mother)
+print(brother)
+print(i)
+
+
+
+print(father_age)
+print(mother_age)
+print(brother_age)
+print(i_age)
+
+
+
+
+print("father name is " + father + " and age " + father_age)
+print("mother name is " + father + " and age " + mother_age)
+print("brother name is " + father + "a nd age " + brother_age)
+print("my name is " + father + " and age " + i_age)
+
+
+
