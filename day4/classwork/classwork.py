be3ed9fee376b07278e70541775874712f19f952
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-05 16:12:47 +0400

    day4

---
+++ day4/classwork/classwork.py
@@ -0,0 +1,57 @@
+# print ("a" > "b")
+
+# print("bob" > "dave")
+
+
+
+
+
+
+
+
+# user_num = int(input("enter any number:"))
+# if user_num > 10:
+#     print("მეტია ათზე")
+# elif user_num == 10:
+#     print("უდრის 10")
+# else:
+#     print("ნაკლებია ათზე")
+
+
+
+
+
+# user_score1 = int(input("enter score N1"))
+# user_score2 = int(input("enter score N2"))
+# user_score3 = int(input("enter score N3"))
+# user_score4 = int(input("enter score N4"))
+
+
+
+# avg_score = (user_score1 + user_score2 + user_score3 + user_score4)/4
+
+
+
+# if avg_score>=9 and avg_score>=10:
+#     print("გილოცავ მატრიცელო შენი ქულაა:" + str(avg_score) + "შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რასაც შეალიე შენი ცხოვრების წლები")
+# elif avg_score<=5 and avg_score>=0:
+#     print("შენი ქულაა" + str(avg_score) + " გილოცავ გაექეცი მატრიცას")
+# elif avg_score>5 and avg_score<9:
+#     print("შენი ქულაა" + str(avg_score) + "you are mid")
+# else:
+#     print("შენი ქულაა" + str(avg_score) + "there is something wrong wiht you")
+
+
+
+
+
+
+
+
+
+
+x = 42
+if x > 5:
+    print("x is greather than 5")
+elif x < 5:
+    print("x is less than 5")
---
+++ day4/homework/homework.py
@@ -0,0 +1,7 @@
+salary = int(input("how much is your salary?:"))
+if salary > 10000:
+    print("Did you study an goal oriented academy?")
+elif salary>1000 and salary<10000:
+    print("you mid")
+elif salary<1000:
+    print("Why did not you join Goa?")
