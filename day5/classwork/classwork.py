9f850f518c0b88a8723da33ab772c5a60439b233
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-05 19:51:20 +0400

    lesson 6

---
+++ day5/classwork/classwork.py
@@ -0,0 +1,31 @@
+# limit = 18
+# age = int(input("enter your age:"))
+# height = int(input("enter your height:"))
+
+# if age > limit and height > 180:
+#     print ("magalic xar da zrdasrulic")
+# elif age > limit and height < 180:
+#     print("zrdasruli xar magram ar xar magali")
+# elif age < limit and height > 180:
+#     print("zrdasruli ar xar magram xar magali")
+# else:
+#     print("vercerti kriteriumi ver daakmayofile")
+
+
+
+
+# if not True:
+#     print("1")
+# elif not ( 1 + 1 == 3):
+#     print("2")
+# else:
+#     print("3")
+
+
+
+#saiterecio cvladi - pudze iteracia (iteration) - ganmeoreba
+i = 1
+while i <= 5:
+    print(i)
+    i += 1                #inkrementacia - zrda
+print("finished")
---
+++ day5/homework/homework.py
@@ -0,0 +1,10 @@
+i = 0
+while i <= 30:
+    i += 1
+
+    if i%2 == 0:
+        print(str(i) + " - luwi")
+    else:
+        print(str(i) + " - kenti")
+
+
