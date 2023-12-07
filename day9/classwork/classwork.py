58b0efc1be844cb6bfedcd22b80e777779d43a2c
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-07 23:01:00 +0400

    lesson10

---
+++ day9/classwork/classwork.py
@@ -0,0 +1,172 @@
+#  nums = [1, 3, 5, 2, 4, 3, 3, 3]
+#  print(len(nums))      #len itvlis elementebs
+
+
+
+#  set_of_nums = set(nums)
+#  print(set_of_nums)            #setebshi dublikatebi ar aris
+
+
+
+
+
+#  letters = ["a", "b", "c"]
+#  letters += ["d"]
+#  print(len(letters))
+#  print(letters)
+
+
+
+
+
+
+# age = 10
+# age +=5
+# print(age)
+
+
+# str = "some text"
+# x = len(str)
+# print(x)
+
+
+
+
+
+# nums = [1, 2, 3]
+# nums.append(4)         #amatebs elementebs siis boloshi
+# print(nums)
+
+
+
+
+# words = ["python", "fun"]
+# words.insert(1, "is")
+# print(words)
+
+
+
+
+
+# family = ["dad", "mom"]
+# family.append("niko")
+# family.insert(2, "brother")
+# print(family)
+
+
+
+# nums = [9, 8, 7, 6, 5]
+# nums.append(4)                    #nums = [9, 8, 7, 6, 5, 4]
+#
+# nums.insert(2, 11)  #nums = [9, 8, 11, 7, 6, 5, 4]
+#
+# print(len(nums))
+
+
+# len aqvs sxvadasxva gamoyeneba
+# 1 - is itvlis siashi elementebis raodenobas
+# 2 - is itvlis stringshi simboloebis raodenobas
+
+
+
+
+
+
+
+
+# letters = ["p", "q", "r", "s", "p", "u"]
+# print(letters.index("r"))
+# print(letters.index("p"))
+# print(letters.index("q"))
+
+
+
+
+# x = [1, 8, 42, 3]
+# print(min(x))
+# print(max(x))
+
+
+
+
+
+# nums = [1, 3, 5, 2, 4]
+# res = min(nums) + max(nums)
+# print(res)
+
+
+
+
+# x = [2, 4, 6, 2, 4, 7, 2, 9]
+# print(x.count(2))      #itvlis siashi ramdnejer aris elementi
+# x.remove(4)
+# print(x)           #shlis konkretul elements siidan
+# x.reverse()          #abrunebs sias ukugma
+# print(x)
+
+
+
+
+
+
+# age = int(input("sheiyvanet asaki"))
+# if age >18:
+#     print("srulwlovani xar")
+# else:
+#     print("bavshvi xar kiddo")
+
+
+
+
+#
+# nums = [2, 4, 8, 9, 5]
+# nums.insert(1, 3)    #4is marcxniv chaiwereba 3     [2, 3, 4, 8, 5]
+#
+#
+# nums.remove(9)       # waishleba 9       [2, 3, 4, 8, 5]
+#
+#
+# nums.insert(0, nums.count(8))      #0 indexsze chaiwereba is ricxvi ramdeni 8 aris siashi     [1, 2, 3, 4, 8, 5]
+#
+#
+# print(nums[3])      #gamoitans mesame indexsze rac iqneba    4
+
+
+
+
+
+
+# nums = [4, 5, 6]
+# msg = "numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
+# print(msg)
+
+
+
+
+
+
+# family = ["vaso", "marina", "nika", "dato"]
+# fl = "ჩემი დედაა: {0}, ჩემი მამაა: {1}, მე ვარ: {2}, ჩემი ძმაა: {3}".format(family[1], family[0], family[2], family[3])
+# print(fl)
+
+
+
+# a = "my name is: {}".format("nika")
+# print(a)
+
+
+# a = "my name is: {} my surname is: {}".format("nika", "zhuzhunadze")
+# print(a)
+
+
+
+# a = "my name is: {1} my surname is: {0}".format("nika", "zhuzhunadze")
+# print(a)
+
+
+
+# a = "my name is: {} my surname is: {}".format("zhuzhunadze", "nika")
+# print(a)
+
+
+
---
+++ day9/homework/homework.py
@@ -0,0 +1,41 @@
+# x = [2, 4, 6, 2, 4, 7, 2, 9]
+# while 4 in x:
+#     x.remove(4)
+# print(x)
+
+
+
+
+family = ["vaso", "marina", "nika", "dato"]
+age = [42, 52, 16, 24]
+
+fl = "ჩემი დედაა {} არის {} წლის".format(family[1], age[0])
+print(fl)
+fl = "ჩემი მამაა {} არის {} წლის".format(family[0], age[1])
+print(fl)
+fl = "მე ვარ {} ვარ {} წლის".format(family[2], age[2])
+print(fl)
+fl = "ჩემი ძმაა {} არის {} წლის".format(family[3], age[3])
+print(fl)
+
+
+print("after 10 years")
+age_after = [52, 62, 26, 34]
+fl = "ჩემი დედაა {} არის {} წლის".format(family[1], age_after[0])
+print(fl)
+fl = "ჩემი მამაა {} არის {} წლის".format(family[0], age_after[1])
+print(fl)
+fl = "მე ვარ {} ვარ {} წლის".format(family[2], age_after[2])
+print(fl)
+fl = "ჩემი ძმაა {} არის {} წლის".format(family[3], age_after[3])
+print(fl)
+
+
+
+
+
+
+
+
+
+
