9c13835bb2c31d95583954fa642bdc42fd8744ce
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-08 21:25:05 +0400

    lesson11

---
+++ day10/classwork/classwork.py
@@ -0,0 +1,65 @@
+# old_list = [2, 4, 6, 2, 4, 7, 2, 9]
+# new_list = set(old_list)
+# print(new_list)              #shlis dublikatebs
+
+
+
+
+
+# old_list = [2, 4, 6, 2, 4, 7, 2, 9]
+# new_list = []
+# for num in old_list:
+#     if num != 4:
+#         new_list.append(num)
+# print(new_list)
+
+
+# x = ", ".join(["spam", "eggs", "ham"])
+# print(x)
+
+
+# x = ", ".join(["spam", "eggs", "ham"])
+# print("ZZ" + x)
+
+
+
+# str = "apple banana tomato strawbery"
+# x = str.split()
+# print(x)
+
+
+
+# str = "apple banana tomato strawbery"
+# x = str.split(" ")
+# print(x)
+# print(x[1])
+
+
+
+# x = "Hello Me"
+# print(x.replace("Me", "world"))       #shecvlis metodi - metodi
+
+
+
+
+
+
+# x = "Hello Me"
+# print(x.replace("o", "x"))
+
+
+
+# x = "Hello Me"
+# print(x.replace("l", ""))
+
+
+
+#def - keywooard romliTc iqmneba funqcia
+#kvadratis_fartobi - funqciis saxeli
+#(gverdis_sigrdze) - funqciis parametri
+def kvadratis_fartobi(gverdos_sigrdze):
+    print(gverdos_sigrdze*gverdos_sigrdze)
+
+
+kvadratis_fartobi(8)
+kvadratis_fartobi(12)
---
+++ day10/homework/homework.py
@@ -0,0 +1,39 @@
+
+
+#davaleba 1
+
+name1 = input("enter your name:")
+name2 = input("enter your name:")
+name3 = input("enter your name:")
+def name(x, y, z):
+    first_name = name1.split(" , ")
+    second_name = name2.split(" , ")
+    third_name = name3.split(" , ")
+    full_name = first_name + second_name + third_name
+    print(full_name)
+
+
+
+name(name1, name2, name3)
+
+
+
+
+#davaleba 2
+
+
+# A = int(input("please enter number"))
+# B = int(input("please enter number"))
+# C = int(input("please enter number"))
+
+# def triangle(a, b, c):
+#     if a + b > c and a + c > b and b + c > a:
+#         print("ეს სამკუთხედი იარსებებს")
+#     else:
+#         print("ეს სამკუთხედი არ იარსებებს")
+
+# triangle(A, B, C)
+
+
+
+
