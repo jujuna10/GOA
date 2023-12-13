4606d478c099d6397ec62af3467c11212e0267f5
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-13 22:30:38 +0400

    day14

---
+++ day14/classwork/classwork.py
@@ -0,0 +1,18 @@
+# name = "nika"
+# print(type(name))
+
+
+
+
+# name = "nika"
+# age = 16
+# age_str = str(age)
+# print("hello my name is",name, "and i",age_str,"years old")
+
+
+
+
+name = "nika"  #anichebs mnishvnelobas cvlads
+print(5 == 4)  # 5 udris 4s
+print(5 != 4)  #5 ar udris 4s
+print(5 < 4)    # 5 naklebia 4ze
---
+++ day14/homework/homework.py
@@ -0,0 +1,136 @@
+1)what is the function used to display text in python?
+answer:
+is used print function because this function dispay text on screen
+
+2)in python which data type is used to store an whole number?
+answer:
+in python for store whole number is used data type integer
+
+3)which of the following data taypes is used to store text in python?
+answer:
+in python for store text is used string data type and all is string what is written in quotation marks
+
+4)what is resut of the operation 10 + 5 * 2 in python?
+answer:
+this operation aswer will 20 because first multipilication will be performed and then addition
+
+5)which of the following is the correct way to assign the value 42 to a variable named answer in python?
+answer:
+answer = 42 because first we creat variable and then assign value 42
+
+6)what is the process of identilfying and fixing errors in a program called?
+answer:
+this is degudding because coding consists of three parts first written code second code running and third debugging
+
+7)which is commonly used in python for naming variables where words are eparated by undercores
+answer:
+for that is used snake_case
+
+8)what is the primary purpose of adding comments to your python code?
+answer:
+comment purpose is that describe code which we wrote
+
+9)how can you take user input in python?
+answer:
+input() and this data we have to save it and we have creat variable where save this information
+
+10)which of the following is not a built-in data type in python?
+answer:
+in python is not built array the same list
+
+11)what function can be used to check the data type of a variable in python?
+answer:
+this function is type() we can pass variable as parameter
+
+12)how can you convert an integer to a string in python?
+answer:
+this we can so str(variable name) this mean in parentheses will be those that will be converting as string
+
+13)what is the term fot converting one data type into another in python?
+answer:
+this term is converting
+
+14)which operator is used to check if two values are equal in python?
+answer:
+this operator is == because = mean assigning a value to a variable
+
+15)what is the result of the logical AND operation between True and False in python?
+answer:
+of this example resut is False because AND's right and left sides too True
+
+16)what will the expression (5 > 3) and (10 < 20) evalute to in python?
+answer:
+result is True because (5 > 3) is true and (10 < 20) is true therefore AND's right and left sides is True and result is True
+
+17)in python what is used to make  dicisions and exectube different code block based on conditions?
+answer:
+in python dicisions and exectube different block based on conditions is used if,elif and else condition operator
+
+18)which type of loop is used to iterate over a sequence (e.g. a list or string) in python?
+answer:
+we can do this by using for and while loops
+
+19)what type of loop is used when you want to repeat a block of code an long as a condicion is True?
+answer:
+this type loof is while loop
+
+20)what does the range() function in python return?
+answer:
+this function will return what we write in parentheses e.g. for list return list's element for string return string's letter and etc
+
+21)which keyboar is used to start "if" statement in python?
+answer:
+
+22)what is the purpose of the "else" statement in python' "if-else" construct
+answer:
+if None of the conditions will be fulfilled then what is written in the else block will be executed
+
+
+23)which data structure is used to store an ordered collection of items in python?
+answer:
+in python to store an ordered collection item is used
+
+24)in python which index represents the first element of a sequence?
+answer:
+print(list name[0])
+
+25)how can you accses the third element of the list in python?
+answerL
+print(list name[2])
+
+26)what does slicing allow you to do with a sequence in python?    ??????
+answer:
+this allows us to remove from the list elements from the range we specified
+
+27)how can you extract a subsquence of a list from index 2 to index 5 (5 must be included) in python?
+answer:
+print(variable name[2:6])
+
+28)
+
+
+
+29)what is reusable block of code in python that performs a specific task called?
+answer:
+this is function
+
+30)in python what are the values passed to a function called?
+answer:
+a value passed to a function is called a arguments
+
+31)which string is used to cnvert a string to upperaces in python?
+answer:
+this method is variable name.upper()
+
+
+32)what list method is used to add an element to the and of a list in python?
+answer:
+this method is variable name.append()
+
+33)in python which kewboard is used to define a new function?
+answer:
+
+34)what keyboard is used to return a value from a function in python?
+from return a value from a function is used return
+
+
