f7acc787dcc2a93687d2e45ed3b82d2346d8ee98
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-23 15:21:45 +0400

    lesson25

---
+++ day24/classwork/classwork.py
@@ -0,0 +1,80 @@
+# #amocana 1
+
+# def list(x):
+#     return x.split(" ")
+
+
+# #amocana 2
+
+
+# def num(x):
+#     return str(x)
+
+
+
+# #amocana 3
+
+
+# def sum(list):
+#     number_sum = 0
+
+#     for i in list:
+#         if i > 0:
+#             number_sum += i
+#     return number_sum
+
+
+
+# #amocana 4
+
+# def score(x):
+#     sum = 0
+#     for i in x:
+#         y = i[0]
+#         z = i[1]
+#         if y > z
+#             sum += 3
+#         elif y < z
+#             sum += 0
+#         else:
+#             sum += 1
+#     return sum
+
+
+
+
+# #amocana 5
+
+
+# def num(x):
+#     sum = 0
+#     for i in range(number):
+#         if i %3 == 0 or i %5 == 0:
+#             sum += i
+#     return sum
+
+
+
+
+
+
+
+#amocana 6
+
+
+
+
+
+
+# def duplicate_count(text):
+
+#     text = text.lower()
+#     arr = []
+
+#     for i in text:
+#         count = text.count(i)
+#         if count > 1:
+#             if i in arr:
+#                 continue
+#             arr.append(i)
+#     return len(arr)
