687aea91a0d08962565053f32698bf636b81e60e
niko zhuzhunadze <jujunaniko@gmail.com>
2023-12-30 22:46:08 +0400

    day27

---
+++ day27/classwork/calculator.py
@@ -0,0 +1,13 @@
+user = (int(input("please enter first number:")))
+user2 = (int(input("please enter second number:")))
+x = input("airchiet moqmedeba:mimateba,gamokleba,gamravleba,gayofa:")
+
+
+if x == "mimateba":
+    print(user + user2)
+elif x == "gamokleba":
+    print(user - user2)
+elif x == "gamravleba":
+    print(user * user2)
+elif x == "gayofa":
+    print(user / user2)
