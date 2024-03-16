a4c14688d2bef4514d2a393ac9fc17f9d0b576be
niko zhuzhunadze <jujunaniko@gmail.com>
2024-03-16 21:02:43 +0400

    day48

---
+++ day48/classwork/script.js
@@ -0,0 +1,5 @@
+console.log(5 + 5)
+console.log(5 - 5)
+console.log(10 * 5)
+console.log(20 / 5)
+console.log(10 ** 2)
---
+++ day48/homework/script.js
@@ -0,0 +1,9 @@
+// davaleba 2                       
+// alert("hello")
+
+// davaleba 3
+
+
+let num1 = 10
+let num2 = 10
+console.log(num1 + num2)
