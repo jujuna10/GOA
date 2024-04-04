891274e0bce57087d466820672e6a341c241cb52
niko zhuzhunadze <jujunaniko@gmail.com>
2024-04-04 19:02:42 +0400

    day52

---
+++ day52/classwork/script.js
@@ -0,0 +1,36 @@
+// const me = {
+//     name: "nika",
+//     lastName: "zhuzhunadze",
+//     age: "17",
+//     school: "#175"
+// }
+
+// const myInfo = {
+//     name: "nika",
+//     lastName: "zhuzhunadze",
+//     age:17,
+// }
+
+// const friendInfo = {
+//     name: "nika",
+//     lastName: "gzirishvili",
+//     age: 17,
+// }
+
+// console.log(myInfo.name);
+// console.log(myInfo.lastName);
+// console.log(myInfo.age);
+
+// console.log(friendInfo.name);
+// console.log(friendInfo.lastName);
+// console.log(friendInfo.age);
+
+
+const me = {
+    name: "nika",
+    lastName: "zhuzhunadze",
+    age: "17",
+}
+
+me.name = "giorgi";
+console.log(me.name);
---
+++ day52/homework/script.js
@@ -0,0 +1,110 @@
+// davaleba 1
+
+// const myInfo = {
+//     name: "nika",
+//     lastName: "zhuzhunadze",
+//     age: 17,
+//     livePlace: "tbilisi",
+//     school: "175"
+// }
+
+// console.log(myInfo.name);
+// console.log(myInfo.lastName);
+// console.log(myInfo.age);
+// console.log(myInfo.livePlace);
+// console.log(myInfo.school);
+
+
+
+// davaleba 2
+
+// const dad = {
+//     name: "vaso",
+//     lastName: "zhuzhunadze",
+//     age: 54
+// }
+
+// const mom = {
+//     name: "marina",
+//     lastName: "abramishvili",
+//     age: 44
+// }
+
+// const brother = {
+//     name: "dato",
+//     lastName: "zhuzhunadze",
+//     age: 25
+// }
+
+// console.log(dad.name)
+// console.log(dad.lastName)
+// console.log(dad.age)
+
+// console.log(mom.name)
+// console.log(mom.lastName)
+// console.log(mom.age)
+
+// console.log(brother.name)
+// console.log(brother.lastName)
+// console.log(brother.age)
+
+
+
+// davaleba 3
+
+// const myInfo = {
+//     name: "nika",
+//     lastName: "zhuzhunadze",
+//     age: 17,
+//     livePlace: "tbilisi",
+//     school: "175"
+// }
+
+// myInfo.name = "giorgi"
+// myInfo.lastName = "giorgadze"
+// myInfo.age = 20
+// myInfo.livePlace = "sachkere"
+// myInfo.school = 200
+// console.log(myInfo.name)
+// console.log(myInfo.lastName)
+// console.log(myInfo.age)
+// console.log(myInfo.livePlace)
+// console.log(myInfo.school)
+
+
+
+// davaleba 4
+
+
+// const myInfo = {
+//     name: "nika",
+//     lastName: "zhuzhunadze",
+//     age: 17,
+//     livePlace: "tbilisi",
+//     school: "175"
+// }
+
+// const myInfo2 = {
+//     name: "giorgi",
+//     lastName: "giorgadze",
+//     age: 20,
+//     livePlace: "sachkere",
+//     school: 200
+// }
+
+// console.log(myInfo == myInfo2)
+
+
+// davaleba 5
+
+// const weather = {
+//     location: "tbilisi",
+//     temperature: "25c",
+//     time: "2 hours",
+//     condition: "მოსალოდნელია წვიმა"
+// }
+
+// console.log(weather.location)
+// console.log(weather.temperature)
+// console.log(weather.time)
+// console.log(weather.condition)
