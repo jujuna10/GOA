3e6364559495b5f1f899c4c229e66d2f53f8a35e
niko zhuzhunadze <jujunaniko@gmail.com>
2024-04-07 20:03:14 +0400

    dat53

---
+++ day53/classwork/script.js
@@ -0,0 +1,56 @@
+// function multiply(num1,num2,num3){
+//     console.log(num1*num2*num3)
+// }
+// multiply(2,3,4)
+
+
+
+// function fullName(name,lastNamee){
+//     // console.log(name + " " + lastNamee)
+// }
+// fullName("nika","zhuzhunadze")
+
+
+
+// function multiply(num1,num2){
+//     const result = (num1*num2)
+//     return result
+// }
+
+
+
+// const mathMultiplay = multiplay(2,4)
+// const result = mathMultiplay * 2
+// console.log(result)
+
+
+
+// function plus(num1,num2){
+//     console.log(num1 + num2)
+// }
+// plus(2,4)
+
+// function minus(num1,num2){
+//     console.log(num1 - num2)
+// }
+// minus(2,4)
+
+// function multiplay(num1,num2){
+//     console.log(num1 * num2)
+// }
+// multiplay(2,4)
+
+// function division(num1,num2){
+//     console.log(num1 / num2)
+// }
+// division(4,2)
+
+
+
+
+function area(width,height){
+    console.log(width*height)
+    console.log(2*(width + height))
+}
+area(10,20 )
+
---
+++ day53/homework/script.js
@@ -0,0 +1,56 @@
+// davaleba 1
+
+// function infoJoin(name , lastName , age ,place ,profession){
+//     console.log(name + lastName + age + place + profession)
+// }
+// infoJoin("nika " , "zhuzhunadze " , 17 , " tbilisi " , "programming")
+
+
+
+// davaleba 2
+
+// function ageCheck(age){
+//     console.log(age > 18)
+//     console.log(age < 18)
+//     console.log(age == 18)
+
+// }
+// ageCheck(20)
+// ageCheck(15)
+// ageCheck(45)
+// ageCheck(10)
+
+
+
+// davaleba 3
+
+// function buttonClick(){
+//     alert("Clicked")
+// }
+
+
+
+// davaleba 4
+
+// const paragraph = document.getElementById("paragraph")
+
+// function colorChanger(){
+//     paragraph.style.color = "green"
+// }
+
+
+
+
+// davaleba 5
+// const text1 = document.getElementById("paragraph1")
+// const text2 = document.getElementById("paragraph2")
+// const text3 = document.getElementById("paragraph3")
+// const text4 = document.getElementById("paragraph4")
+
+// function colorChange(){
+//     text1.style.color = "green"
+//     text2.style.color = "green"
+//     text3.style.color = "green"
+//     text4.style.color = "green"
+// }
+// colorChange()
