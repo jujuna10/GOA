c7da001f8a88fb2ac346b87bd65ce376eb1d1288
niko zhuzhunadze <jujunaniko@gmail.com>
2024-04-14 16:42:37 +0400

    day55

---
+++ day55/classwork/main.py
@@ -0,0 +1,71 @@
+# number1 = int(input("please enter number1:"))     # input funqciit momxmarebels sheaqvs pirveli ricxvi
+# number2 = int(input("please enter number2:"))     # meore ricxvi sheaqvs
+#
+# print(number1 + number2)  # terminalshi gamoaqvs mimateba
+# print(number1 - number2)  # terminalshi gamoaqvs gamokleba
+# print(number1 * number2)  # terminashi gamoaqvs gamravleba
+# print(number1 / number2)  # terminalshi gamoaqvs gayofa
+#
+#
+# # print funqciit gamogvaqvs terminalshi rac gvinda
+# # input funqciit momxmarebels sheyavs monacemebia
+
+
+
+# davaeba 1
+
+# first = 10
+# second = 20
+# third = 30
+# if first + second > third:
+#     print("arsebobs")
+# elif first + third > second:
+#     print("arsebobs")
+# elif second + third > first:
+#     print("arsebobs")
+# else:
+#     print("ar arsebobs")
+#
+#
+# # davaleba 2
+#
+# num1 = 10
+# num2 = 20
+#
+# def plus(x,y):
+#     print(x + y)
+# plus(num1,num2)
+#
+# def minus(x,y):
+#     print(x - y)
+# minus(num1,num2)
+#
+# def multiplay(x,y):
+#     print(x * y)
+# multiplay(num1,num2)
+#
+# def division(x,y):
+#     print(x / y)
+# division(num1,num2)
+#
+# def xarisxi(x,y):
+#     print(x ** y)
+# xarisxi(num1,num2)
+
+
+# davaleba 3
+
+# number = 5
+# factorial = 1
+# for i in range(1,number+1):
+#     factorial = factorial * i
+# print(factorial)
+
+
+
+num1 = 5
+num2 = 10
+num3 = num1 + num2
+print(num3)
+num3 = num1 * num2
+print(num3)
---
+++ day55/homework/main.py
@@ -0,0 +1,107 @@
+# davaleba 1
+#
+# num1 = 10
+# num2 = 20
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 + num2)
+# print(num1 / num2)
+
+
+
+# davaleba 2
+
+# book1 = "title 1"
+# book2 = "title 2"
+# book3 = "title 3"
+# book4 = "title 4"
+# book5 = "title 5"
+# book6 = "title 6"
+# book7 = "title 7"
+# book8 = "title 8"
+# book9 = "title 9"
+# book10 = "title 10"
+#
+# price1 = 20.0
+# price2 = 25.0
+# price3 = 15.0
+# price4 = 30.0
+# price5 = 35.0
+# price6 = 28.0
+# price7 = 18.0
+# price8 = 12.0
+# price9 = 10.0
+# price10 = 22.0
+#
+# def sale10(*argv):
+#
+#     new_price1 = argv[0] * 0.10
+#     print(argv[0] - new_price1)
+#
+#     new_price2 = argv[1] * 0.10
+#     print(argv[1] - new_price2)
+#
+#     new_price3 = argv[2] * 0.10
+#     print(argv[2] - new_price3)
+#
+#     new_price4 = argv[3] * 0.10
+#     print(argv[3] - new_price4)
+#
+#     new_price5 = argv[4] * 0.10
+#     print(argv[4] - new_price5)
+# sale10(price1,price2,price3,price4,price5)
+#
+# def sale20(*argv):
+#
+#     new_price6 = argv[0] * 0.10
+#     print(argv[0] - new_price6)
+#
+#     new_price7 = argv[1] * 0.10
+#     print(argv[1] - new_price7)
+#
+#     new_price8 = argv[2] * 0.10
+#     print(argv[2] - new_price8)
+#
+#     new_price9 = argv[3] * 0.10
+#     print(argv[3] - new_price9)
+#
+#     new_price10 = argv[4] * 0.10
+#     print(argv[4] - new_price10)
+# sale20(price6,price7,price8,price9,price10)
+
+
+
+# davaleba 3
+
+# age = int(input("enter your age:"))
+# future_age = (age + 25)
+# print(f"25 wlis shemdeg iqnebi {future_age} wlis")
+
+
+
+# davaleba 4
+
+# name = input("please enter your name:")
+# lastname = input("please enter your lastname:")
+# age = int(input("please enter your age:"))
+# dwelling_place = input("your city:")
+# profession = input("enter your profession:")
+# print("your name is {} lastname is {} you are {} age,you live in {} and you are {}".format(name,lastname,age,dwelling_place,profession))
+
+
+
+# davaleba 5
+
+
+
+# number = int(input("please enter number:"))
+# def even(x):
+#     if x % 2 == 0:
+#         print("this number is even")
+#     else:
+#         print("this number is odd")
+#
+# even(number)
+
+
+
