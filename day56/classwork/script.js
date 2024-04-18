f7b0eae07c9a6b862069da24117e0cf880fda5df
niko zhuzhunadze <jujunaniko@gmail.com>
2024-04-18 20:53:43 +0400

    day56

---
+++ day56/classwork/script.js
@@ -0,0 +1,22 @@
+// const numberOne = 10;
+
+// function mathh(num){
+//     let multiplay = (num * 2);
+//     let divide = (num / 2);
+//     let quality = (num ** 2);
+//     console.log(multiplay);
+//     console.log(divide);
+//     console.log(quality)
+
+
+// }
+// mathh(numberOne)
+
+
+
+let name = prompt("enter your name");
+let lastName = prompt("enter yout lastname");
+let age = prompt("enter your age");
+let place = prompt("enter your Dwelling place");
+let fullInfo = (name + " " + lastName + " " + age + " " + place)
+console.log(fullInfo)
---
+++ day56/homework/script.js
@@ -0,0 +1,45 @@
+// davaleba 1
+
+// const number1 = prompt("enter first number:");
+// const number2 = prompt("enter second number:");
+// function mathOperation(first,second){
+//     let num1 = Number(first);
+//     let num2 = Number(second)
+//     console.log(num1 + num2);
+//     console.log(num1 - num2);
+//     console.log(num1 * num2);
+//     console.log(num1 / num2);
+// }
+// mathOperation(number1,number2)
+
+
+
+// davaleba 2
+
+
+function userInfo(){
+    event.preventDefault(event)
+
+
+    const name = document.getElementById("name").value;
+    console.log(name);
+
+    const lastName = document.getElementById("lastname").value;
+    console.log(lastName);
+
+    const age = document.getElementById("age").value;
+    console.log(age);
+}
+
+
+// davaleba 3
+
+// function inputText(){
+//     let text = document.getElementById("text").value;
+//     let color = document.getElementById("color").value;
+//     let paragraph = document.getElementById("p");
+
+//     paragraph.textContent = text;
+//     paragraph.style.color = color;
+
+// }
