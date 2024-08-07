0d6eba7c10cc1f3b6ff6e23a1697fdd9cac33b36
jujuna10 <jujunaniko@gmail.com>
2024-08-07 18:56:46 +0400

    commit

---
+++ level102(ჯგუფი 0)/classwork/script.js
@@ -0,0 +1,4 @@
+const _ = require("lodash")
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+const shuffled = _.shuffle(numbers)
+console.log(shuffled)
---
+++ level104(ჯგუფი 0)/classwork/main.js
@@ -0,0 +1,22 @@
+// import { greet } from "./script.js";
+// import { sum } from "./script2.js";
+
+// greet("nika")
+// sum(10,10)
+
+// const lodash = require("lodash")
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log(lodash.shuffle(arr))
+
+// const takeRight = require("lodash")
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log(takeRight.takeRight(arr,4))
+
+// const zip = require("lodash")
+// let zipped = zip.zip(['a', 'b'], [1, 2], [true, false]);
+// console.log(zipped)
+
+// const integer = require("lodash")
+// let isInteger = integer.isInteger(10)
+// console.log(isInteger)
+
