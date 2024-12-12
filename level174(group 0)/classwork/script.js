375ddc45a14028bba7278ba3a98d72338fd3fd23
jujuna10 <jujunaniko@gmail.com>
2024-12-12 20:46:02 +0400

    commit

--- level174(group 0)/classwork/script.js
+++ level174(group 0)/classwork/script.js
@@ -10,7 +10,7 @@ const filter = (input,callback) => {
     if(input === 1 || input === 2 || input === 3 || input === 4 || input === 5){
         callback(null,input)
     }else{
-        const err = new Error('input is not correct')
+        const err = new Error('input is not correctt')
         callback(err,input)
     }
 }
